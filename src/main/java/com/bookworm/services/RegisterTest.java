2b7be4e7a14eb73d7a5600bdd920cbfb42a0ba47
lacquer <lacquer@Demidly>
2017-02-24 12:29:30 -0400

    First commit
    - made test for email and phone#

---
+++ src/main/java/com/bookworm/services/RegisterTest.java
@@ -0,0 +1,34 @@
+package com.bookworm.services;
+
+public class RegisterTest {
+	
+	public RegisterTest(){
+		
+	}
+	
+	/* Check for one '@', check that @ isnt at start or end, check
+	 * for at least one '.', return false if any test fails
+	 */
+	public static boolean testEmail(String email) {
+		
+		if (email.indexOf('@', email.indexOf('@')) != -1)
+			return false;
+		else if (email.indexOf('@') < 1)
+			return false;
+		else if (email.indexOf('@') == email.length() - 1)
+			return false;
+		else if (email.indexOf('.') < 0)
+			return false;
+		
+		else return true;
+	}
+	
+	public static boolean testPhone(String phone) {
+		
+		if (phone.length() < 7)
+			return false;
+		else if (phone.matches(".*[a-zA-Z].*"))
+			return false;
+		else return true;
+	}
+}
